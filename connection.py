1517336edc569e4ead2387e1d1685e8324b59151
Zoeinfp <zoebelleton@gmail.com>
2018-07-20 11:24:39 +0200

    2nd step : Connect to database

---
+++ connection.py
@@ -0,0 +1,21 @@
+import pymysql
+
+
+class Connection:
+    @staticmethod
+    def connect_to_database():
+        connection = pymysql.connect(host="127.0.0.1",  # The Host
+                                     user="root",  # username
+                                     passwd="P0rt1she@d",  # password
+                                     db="openfoodfact",
+                                     port=3306)  # name of the data base
+
+        cursor = connection.cursor()
+        print("Vous êtes connecté")
+        return connection, cursor
+
+    @staticmethod
+    def close_database_connection(cursor, connection):
+        # MySQL connection
+        cursor.close()
+        connection.close()
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 #!/usr/local/bin/python
+from connection import Connection
 
-if __name__ == "__main__":
+
+def make_a_choice():
     print("Hi ! Do you want 1 = to substitute a product  or 2 = to find substituted products ?")
     input_choice = input("Enter the number")
 
     choice = int(input_choice)
     if choice == 1:
-        print("You can now substitute a product")
+        connection, cursor = Connection.connect_to_database()
+        Connection.close_database_connection(cursor, connection)
     elif choice == 2:
         print("You can now find substituted products")
+    else:
+        print("Please try again...")
+        make_a_choice()
+
+
+if __name__ == "__main__":
+    make_a_choice()
